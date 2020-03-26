138c1c165001eec99577111dcb3406dd83d1e53a
FabienMarti <martiretti.fabien@gmail.com>
2020-03-26 16:07:29 +0100

    Ajout du php, des dossier pour un arborescence clair

---
+++ pages/CSS.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="../styles/style.css" type="text/css" />
+    <title>📃 Memento CSS 📃</title>
+</head>
+<header>
+    <nav id="navMenu">
+        <fieldset id="navFieldSet">
+            <legend>Menu de Navigation</legend>
+            <ul>
+                <li><strong>Structure générale</strong>
+                    <ol>
+                        <li><a href="#head"></a></li>
+                        <li><a href="#body"></a></li>
+                        <li><a href="#foot"></a></li>
+                    </ol>
+                </li>
+                <li><strong>Les formulaires</strong>
+                    <ol>
+                        <li><a href="#formElem">L'élement Form</a></li>
+                        <li><a href="#inputElem">L'élement Input</a></li>
+                        <ul>
+                            <li><a href="#inputImportant">Important</a></li>
+                            <li><a href="#typeText">Les types Text et Nom</a></li>
+                            <li><a href="#button">Les boutons</a></li>
+                        </ul>
+                    </ol>
+                </li>
+                <li><strong>Les tableaux</strong>
+                    <ol>
+                        <li><a href="#mainTags">Balises principales</a></li>
+                        <li><a href="#tags">Balises internes au tableau</a></li>
+                    </ol>
+                </li>
+            </ul>
+            <a href="#haut">
+                <image id="flechehaut" src="../images/fleche-haut.png" />
+            </a>
+        </fieldset>
+    </nav>
+</header>
+
+<body>
+
+
+
+</body>
+
+</html>
---
+++ php/header.php
@@ -0,0 +1,7 @@
+
+<a href="HTML.php">
+    <image class="imgHtml" src="../images/HTML5_Logo.png" />
+</a>
+<a href="CSS.php">
+    <image class="imgCSS" src="../images/CSS3_Logo.png" /> 
+</a>
