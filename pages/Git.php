6be43224e690f39e61d2830b2171bd60d8258722
FabienMarti <martiretti.fabien@gmail.com>
2020-03-27 09:45:41 +0100

    ajout page Git, styles pour Git et liens

---
+++ pages/Git.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="../styles/style.css" type="text/css" />
+    <title>📃 Memento Git/GitHub 📃</title>
+</head>
+<header>
+    <nav id="navMenu">
+        <fieldset id="navFieldSet">
+            <legend>Menu de Navigation</legend>
+            <ul>
+                <li><strong>Structure générale</strong>
+                    <ol>
+                        <li><a href="#head"></a></li>
+                        <li><a href="#body"></a></li>
+                        <li><a href="#foot"></a></li>
+                    </ol>
+                </li>
+                <li><strong>Les attributs</strong>
+                    <ol>
+                        <li><a href="#formElem"></a></li>
+                        <li><a href="#inputElem"></a></li>
+                        <ul>
+                            <li><a href="#inputImportant"></a></li>
+                            <li><a href="#typeText"></a></li>
+                            <li><a href="#button"></a></li>
+                        </ul>
+                    </ol>
+                </li>
+                <li><strong></strong>
+                    <ol>
+                        <li><a href="#mainTags"></a></li>
+                        <li><a href="#tags"></a></li>
+                    </ol>
+                </li>
+            </ul>
+            <a href="#haut">
+                <image id="flechehaut" src="../images/fleche-haut.png" />
+            </a>
+        </fieldset>
+    </nav>
+    <div><?php include("../php/header.php"); ?></div>
+</header>
+<body>
+    
+    <aside id="sideInfos">
+        <fieldset>
+            <legend>C'est quoi le GitHub ?</legend>
+            <p>GitHub est un service web d'hébergement et de gestion de développement de logiciels, utilisant le logiciel de gestion de versions Git. Ce site est développé en Ruby on Rails et Erlang par Chris Wanstrath, PJ Hyett et Tom Preston-Werner. GitHub propose des comptes professionnels payants, ainsi que des comptes gratuits pour les projets de logiciels libres. Le site assure également un contrôle d'accès et des fonctionnalités destinées à la collaboration comme le suivi des bugs, les demandes de fonctionnalités, la gestion de tâches et un wiki pour chaque projet.</p>
+        </fieldset>
+    </aside>
+    <section>
+        <fieldset>
+        <legend class="bigTitle">Commandes UNIX</legend>
+        <p>Pour pouvoir utiliser Git à sont plein potentiel, il faut maitriser les commandes UNIX dans un terminal (ou GitBash pour windows), plusieurs commandes vont servir pour préparer le terrain pour Git : </p>
+        <ol>
+            <li>UNIX sans Git</li>
+        
+            <ul>
+                <li>"pwd" :  print working directory, cette commande nous montre ou nous nous situons,</li>
+                <li>"cd" : change directory, peut être utilisée pour se déplacer dans les dossier, de plus l'utilisation des ".." apres "cd" permet de remonter l'arborescence des dossiers,</li>
+                <li>"mkdir" : make directory, est utilisée pour créér un dossier suivit du nom de celui-ci,</li>
+                <li>"touch" est utilisé pour créér un fichier dans le dossier actuel en n'oubliant pas l'extension de se fichier,</li>
+                <li>"ls" : list, permet de lister le contenu du dossier actuel,</li>
+            </ul>
+            <li>Commandes Git</li>
+            <ul>
+                <li></li>
+            
+            </ul>
+        </ol>
+        </section>
+    </fieldset>
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
--- php/header.php
+++ php/header.php
@@ -1,4 +1,5 @@
 
+<a href="Git.php"><image class="imgGit" src="../images/git_Logo.png" /></a>
 <a href="HTML.php"><image class="imgHtml" src="../images/HTML5_Logo.png" /></a>
 <a href="CSS.php"><image class="imgCSS" src="../images/CSS3_Logo.png" /></a>
 <a href="PHP.php"><image class="imgphp" src="../images/php_Logo.png" /></a>
