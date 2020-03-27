dd6b148564d80c8b729d510f4920f22f0fc1ecd4
FabienMarti <martiretti.fabien@gmail.com>
2020-03-27 14:08:00 +0100

    Ajout du CSS direcement sur la feuille php header

--- php/header.php
+++ php/header.php
@@ -1,4 +1,35 @@
+<style type="text/css">
+.imgHtml {
+    width: 100px;
+    height: 100px;
+    background-size: cover;
+}
 
+.imgCSS {
+    width: 75px;
+    height: 100px;
+    background-size: cover;
+}
+
+.imgphp {
+    width: 75px;
+    height: 50px;
+    padding: 20px;
+    background-size: cover;
+}
+
+.imgJS {
+    width: 75px;
+    height: 80px;
+    background-size: cover;
+}
+
+.imgGit {
+    width: 80px;
+    height: 80px;
+    background-size: cover;
+}
+</style>
 <a href="Git.php"><image class="imgGit" src="../images/git_Logo.png" /></a>
 <a href="HTML.php"><image class="imgHtml" src="../images/HTML5_Logo.png" /></a>
 <a href="CSS.php"><image class="imgCSS" src="../images/CSS3_Logo.png" /></a>
