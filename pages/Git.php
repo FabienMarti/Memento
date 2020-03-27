28cb2d2d127592ea161b0b308047c8198afdeeb8
FabienMarti <martiretti.fabien@gmail.com>
2020-03-27 10:26:20 +0100

    complétion page Git

--- pages/Git.php
+++ pages/Git.php
@@ -11,22 +11,15 @@
         <fieldset id="navFieldSet">
             <legend>Menu de Navigation</legend>
             <ul>
-                <li><strong>Structure générale</strong>
+                <li><strong>Généralités UNIX</strong>
                     <ol>
-                        <li><a href="#head"></a></li>
-                        <li><a href="#body"></a></li>
-                        <li><a href="#foot"></a></li>
+                        <li><a href="#commands">Les commandes</a></li>
                     </ol>
                 </li>
-                <li><strong>Les attributs</strong>
+                <li><strong>Utilisation de Git</strong>
                     <ol>
-                        <li><a href="#formElem"></a></li>
-                        <li><a href="#inputElem"></a></li>
-                        <ul>
-                            <li><a href="#inputImportant"></a></li>
-                            <li><a href="#typeText"></a></li>
-                            <li><a href="#button"></a></li>
-                        </ul>
+                        <li><a href="#gitInit">Initialiser son Git</a></li>
+                        <li><a href="#gitEdit">Modifier un fichier</a></li>
                     </ol>
                 </li>
                 <li><strong></strong>
@@ -53,38 +46,52 @@
     </aside>
     <section>
         <fieldset>
-        <legend class="bigTitle">Commandes UNIX</legend>
-        <p>Pour pouvoir utiliser Git à sont plein potentiel, il faut maitriser les commandes UNIX dans un terminal (ou GitBash pour windows), plusieurs commandes vont servir pour préparer le terrain pour Git : </p>
-        <ol>
-            <li>UNIX sans Git</li>
-        
-            <ul>
-                <li>"pwd" :  print working directory, cette commande nous montre ou nous nous situons,</li>
-                <li>"cd" : change directory, peut être utilisée pour se déplacer dans les dossier, de plus l'utilisation des ".." apres "cd" permet de remonter l'arborescence des dossiers,</li>
-                <li>"mkdir" : make directory, est utilisée pour créér un dossier suivit du nom de celui-ci,</li>
-                <li>"touch" est utilisé pour créér un fichier dans le dossier actuel en n'oubliant pas l'extension de se fichier,</li>
-                <li>"ls" : list, permet de lister le contenu du dossier actuel,</li>
-            </ul>
-            <li>Commandes Git</li>
-            <ul>
-                <li></li>
-            
-            </ul>
-        </ol>
-        </section>
-    </fieldset>
-
-
-
-
-
-
-
+            <legend class="bigTitle">Généralités UNIX</legend>
+            <h1 id="commands">Les commandes</h1>
+            <p>Pour pouvoir utiliser Git à sont plein potentiel, il faut maitriser les commandes UNIX dans un terminal (ou GitBash pour windows), plusieurs commandes vont servir pour préparer le terrain pour Git : </p>
+            <ol>
+                <li>UNIX sans Git</li>           
+                <ul>
+                    <li>"pwd" :  print working directory, cette commande nous montre ou nous nous situons,</li>
+                    <li>"cd" : change directory, peut être utilisée pour se déplacer dans les dossier, de plus l'utilisation des ".." apres "cd" permet de remonter l'arborescence des dossiers,</li>
+                    <li>"mkdir" : make directory, est utilisée pour créér un dossier suivit du nom de celui-ci,</li>
+                    <li>"touch" est utilisé pour créér un fichier dans le dossier actuel en n'oubliant pas l'extension de se fichier,</li>
+                    <li>"ls" : list, permet de lister le contenu du dossier actuel,</li>
+                </ul>
+                <li>Commandes Git</li>
+                <ul>
+                    <li>"git init" permet d'initialiser Git dans un dossier caché ( .git) à l'endroit de l'init</li>
+                    <li>"git add" permet d'ajouter des fichiers nouveaux ou modifiés</li>
+                    <li>"git commit -m " " permet d'ajouter un commentaire sur l'upload suivant</li>
+                    <li>"git remote" défini le repository cible sur le site de GitHub (on utilisera en général un lien préparé sur le repository cible)</li>
+                    <li>"git push -u master" permet d'upload les fichiers précédement sélectionnés pour la mise a jour sur le site GitHub et permet le stockage dans des serveurs distants                
+                </ul>
+            </ol>
+        </fieldset>
+    </section>
+    <section>
+        <fieldset>
+            <legend class="bigTitle">Utilisation de Git</legend>
+            <h1 id="gitInit">Initialiser son Git</h1>
+            <p>Afin de pouvoir utiliser Git, nous devons l'initialiser dans le dossier racine de notre site web (local) et ensuite permettre un controle distant pour l'envoie et la reception de données, voici la marche à suivre : </p>
+            <ol>   
+                <li>Créer un repository sur GitHub</li>
+                <li>Se placer à la racine du site web local(soit par commandes avec "cd" soit avec un clic droit dans le dossier et ouvrir un terminal içi)</li>
+                <li>git init</li>
+                <li>git add lesPremiersFichiersaEnvoyer.extensionDuFichier</li>
+                <li>git commit -m "Un commentaire pertinent"</li>
+                <li>git remote .... , on utilise un lien disponible sur la page du repository en question, on colle avec shift+insert</li>
+                <li>git push -u origin master</li>
+            </ol> 
+            <p>Git est initialisé et prêt à l'emploi !</p>
+            <h1 id="gitEdit">Modifier un fichier</h1>
+            <p>Après la modification d'un fichier en local, il est important de le partager sur son Git ne serais-ce que pour garder une copie en lieu sur, pour ce faire nous utilisons des commandes dans un terminal ouvert à la racine de notre site web local : </p>
+            <ol>
+                <li>git add : on ajoute les fichier modifiés ou les dossiers</li>
+                <li>git commit -m " " : on laisse un commentaire de mise à jour</li>
+                <li>git push -u origin master : on envoie au serveur distant</li>
+            </ol>
+        </fieldset>
+    </section>
 </body>
-
-
-
-
-
-
 </html>
